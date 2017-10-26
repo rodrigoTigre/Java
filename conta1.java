a0a0997d06f6c26c32be1383fab674bddba00d1e
rodrigoTigre <32077587+rodrigoTigre@users.noreply.github.com>
2017-10-26 17:03:27 +0100

    Add files via upload

---
+++ conta1.java
@@ -0,0 +1,11 @@
+package Pacote1;
+
+public class conta1 {
+ public static void main(String[] args) {
+    System.out.printf("%d\n",4+3*(15-2));
+    System.out.printf("%f\n",40.0/9);
+	System.out.printf("%d\n",40%3);
+
+	}
+
+}
---
+++ primeiroprograma.java
@@ -0,0 +1,6 @@
+package Pacote1;
+public class primeiroprograma {
+	public static void main(String[] args) {
+      System.out.println("hello world");
+	}
+}
