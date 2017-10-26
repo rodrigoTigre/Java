d592139dc0910acd06c51c5c426753299d23be91
rodrigoTigre <32077587+rodrigoTigre@users.noreply.github.com>
2017-10-26 17:27:42 +0100

    Add files via upload

---
+++ leia1.java
@@ -0,0 +1,18 @@
+package Pacote1;
+import java.io.*;
+public class leia1 {
+
+	public static void main(String[] args) 
+	
+	{
+	
+		{string c="";
+		System.out.print("Digite um digito");
+		ImputStreamReader LerS = new ImputStreamReader(System.in);
+		BufferedReader GuardaS = new BufferedReader (LerS);
+		c = GuardaS.readLine();
+		System.out.printf("Digitou :%d", Integer.valueOF(c)); }}}
+		
+	
+				
+
