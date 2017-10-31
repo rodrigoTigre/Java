83fbc5783f568a116174e8121995828e0fc9280b
rodrigoTigre <32077587+rodrigoTigre@users.noreply.github.com>
2017-10-31 08:39:57 +0000

    Update Retangulo.java

--- Retangulo.java
+++ Retangulo.java
@@ -3,7 +3,18 @@ package projeto1;
 public class Retangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Qual o Comprimento?");
+			String C = "";
+			InputStreamReader LerS = new InputStreamReader (System.in);
+			BufferedReader GuardaS = new BufferedReader (LerS);
+			C = GuardaS.readLine();
+		System.out.println("Qual a Largura?");
+			String L = "";
+			InputStreamReader LerSL = new InputStreamReader (System.in);
+			BufferedReader GuardaSL = new BufferedReader (LerSL);
+			L = GuardaSL.readLine();
+		System.out.printf("A Área é %d.", Integer.valueOf(C)*Integer.valueOf(L));
 
 	}
 
