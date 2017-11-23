1796fa38587dbd82c88b812ea96b5873f21665af
rodrigoTigre <32077587+rodrigoTigre@users.noreply.github.com>
2017-11-23 17:04:11 +0000

    Add files via upload

---
+++ problema2.java
@@ -0,0 +1,29 @@
+package problema2;
+
+import java.util.Scanner;
+
+public class problema2 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        
+        double num1,num2,total;
+        double monthstring = 0;
+        String operador;
+        System.out.println("numero 1:");
+        num1 = ler.nextDouble();
+        System.out.println("numero 2:");
+        num2 = ler.nextDouble();
+        System.out.println("qual o operador?:");
+        operador = ler.next();
+        switch (operador) {
+        case "*" : monthstring = (num1*num2);
+    	case "+" : monthstring = (num1+num2);
+    	case "-" : monthstring= (num1-num2);
+    	case "/" : monthstring = (num1/num2);
+        }
+        System.out.println(+monthstring);
+    }}
+
+
